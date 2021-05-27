dd607954899e862a231a68469f85661ea2b0da31
cqpmyd <47035258+cqpmyd@users.noreply.github.com>
2021-05-27 22:39:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-git clone https://github.com/kenzok8/openwrt-packages.git package/
-git clone https://github.com/kenzok8/small.git package/
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
