2e82607fb01573bbae660a8780ddc17d3488ba80
cqpmyd <47035258+cqpmyd@users.noreply.github.com>
2021-05-27 12:10:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/kenzok8/openwrt-packages.git lede/package
+git clone https://github.com/kenzok8/small.git lede/package
